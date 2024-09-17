038050f765012f804479aec990656253a421e345
Jeff Page <jeff@nextnode.ca>
2024-09-17 09:08:03 -0400

    Add project

---
+++ FSharpConsole/Main.fs
@@ -0,0 +1,15 @@
+﻿module Main
+
+open System
+open DataWrangle
+
+[<EntryPoint>]
+let main (args : string[]) =
+    if args.Length < 2 then 1   
+    else
+        let clientDataPath = args[0]
+        let claimDataPath = args[1]
+        let result = wrangleClientData clientDataPath // eg "./Data/Clients_20240912.csv"
+        let result = wrangleClaimData  claimDataPath // eg "./Data/Claims_20240912.csv"
+
+        0
